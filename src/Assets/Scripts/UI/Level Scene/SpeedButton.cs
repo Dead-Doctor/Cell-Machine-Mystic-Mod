0b69a7c23f103a4d72c4d5cdce864569fb96fc99
DeadDoctor <bkloth06@gmail.com>
2021-08-12 17:38:54 +0200

    Added a speed button

---
+++ src/Assets/Scripts/UI/Level Scene/SpeedButton.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpeedButton : MonoBehaviour
+{
+    public Sprite slowSprite;
+    public Sprite normalSprite;
+    public Sprite fastSprite;
+    public Image image;
+
+    private float slowSpeed = 1f;
+    private float normalSpeed = .2f;
+    private float fastSpeed = .04f;
+
+    public void changeSpeed()
+    {
+        if (GridManager.animationLength == normalSpeed)
+        {
+            GridManager.animationLength = fastSpeed;
+            image.sprite = fastSprite;
+        }
+        else if (GridManager.animationLength == fastSpeed)
+        {
+            GridManager.animationLength = slowSpeed;
+            image.sprite = slowSprite;
+        } 
+        else
+        {
+            GridManager.animationLength = normalSpeed;
+            image.sprite = normalSprite;
+        }
+    }
+}
